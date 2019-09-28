945cd836abb918d041e815d8d863d2d1c58629df
Ankush Patel <apatel79@vols.utk.edu>
2019-09-28 07:17:20 -0400

    update client

--- bt/rfcomm-client.c
+++ bt/rfcomm-client.c
@@ -6,39 +6,28 @@
 
 int main(int argc, char **argv)
 {
-    struct sockaddr_rc loc_addr = { 0 }, rem_addr = { 0 };
-    char buf[1024] = { 0 };
-    int s, client, bytes_read;
-    socklen_t opt = sizeof(rem_addr);
+    struct sockaddr_rc addr = { 0 };
+    int s, status;
+    char dest[18] = "A8:7D:12:04:61:C0";
 
-    // allocate socket
+    // allocate a socket
     s = socket(AF_BLUETOOTH, SOCK_STREAM, BTPROTO_RFCOMM);
 
-    // bind socket to port 1 of the first available 
-    // local bluetooth adapter
-    loc_addr.rc_family = AF_BLUETOOTH;
-    loc_addr.rc_bdaddr = *BDADDR_ANY;
-    loc_addr.rc_channel = (uint8_t) 1;
-    bind(s, (struct sockaddr *)&loc_addr, sizeof(loc_addr));
+    // set the connection parameters (who to connect to)
+    addr.rc_family = AF_BLUETOOTH;
+    addr.rc_channel = (uint8_t) 1;
+    str2ba( dest, &addr.rc_bdaddr );
 
-    // put socket into listening mode
-    listen(s, 1);
+    // connect to server
+    status = connect(s, (struct sockaddr *)&addr, sizeof(addr));
 
-    // accept one connection
-    client = accept(s, (struct sockaddr *)&rem_addr, &opt);
-
-    ba2str( &rem_addr.rc_bdaddr, buf );
-    fprintf(stderr, "accepted connection from %s\n", buf);
-    memset(buf, 0, sizeof(buf));
-
-    // read data from the client
-    bytes_read = read(client, buf, sizeof(buf));
-    if( bytes_read > 0 ) {
-        printf("received [%s]\n", buf);
+    // send a message
+    if( status == 0 ) {
+        status = write(s, "hello!", 6);
     }
 
-    // close connection
-    close(client);
+    if( status < 0 ) perror("uh oh");
+
     close(s);
     return 0;
 }
