87492a00f1d45601cdbe629956bf43e2a786aed1
Ankush Patel <apatel79@vols.utk.edu>
2019-09-28 13:30:41 -0400

    update particles

--- hack2019/src/app/app.module.ts
+++ hack2019/src/app/app.module.ts
@@ -4,14 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ParticlesModule } from 'angular-particle';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    MatSliderModule,
+    MatProgressSpinnerModule,
+    ParticlesModule,
     BrowserAnimationsModule
   ],
   providers: [],
---
+++ hack2019/src/app/home/home.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+  myStyle: object = {};
+  myParams: object = {};
+  width: number = 100;
+  height: number = 100;
+
+  ngOnInit() {
+    this.myStyle = {
+        'position': 'fixed',
+        'width': '100%',
+        'height': '100%',
+        'z-index': -1,
+        'top': 0,
+        'left': 0,
+        'right': 0,
+        'bottom': 0,
+    };
+
+    this.myParams = {
+        particles: {
+            number: {
+                value: 200,
+            },
+            color: {
+                value: '#ff0000'
+            },
+            shape: {
+                type: 'triangle',
+            },
+        }
+    };
+  }
+}
