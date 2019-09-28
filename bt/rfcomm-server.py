7945736322ad07b00d00a58676aed332718b2189
= <apatel79@vols.utk.edu>
2019-09-28 08:41:21 -0400

    update server py

--- bt/rfcomm-server.py
+++ bt/rfcomm-server.py
@@ -9,8 +9,12 @@ server_sock.listen(1)
 client_sock,address = server_sock.accept()
 print "Accepted connection from ",address
 
-data = client_sock.recv(1024)
-print "received [%s]" % data
+while 1:
+    data = client_sock.recv(1024)
+    print "received [%s]" % data
+
+    if data == 'stop':
+        break
 
 client_sock.close()
 server_sock.close()
