2ce3d39df4345d507926d2b878552c055d51eccc
Chase Brunette-Pak <chasebrunettepak@gmail.com>
2019-09-28 16:11:56 -0400

    Allowed client side to recieve sensor output

--- bt/clientReader.py
+++ bt/clientReader.py
@@ -1,4 +1,4 @@
-import bluetooth
+import bluetooth, json, os.path
 
 # Set up for client to listen from server
 server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
@@ -6,15 +6,25 @@ server_sock.bind(("",2))
 server_sock.listen(2)
 client_sock,address = server_sock.accept()
 
-print "Accepted connection from ",address
-
+# Constantly listens for input
 while 1:
 
-    # Reads data from server
-    data = client_sock.recv(1024)
-    print "received [%s]" % data
-    if data == 'stop':
-        break
+    # Checks and sees if GUI has read json file
+    if not os.path.exists('log.json'):
+
+        # If GUI has, make new file
+        with open("log.json", "w") as f:
+            buf = {}
+
+            # Splice incoming data
+            data = client_sock.recv(1024)
+            data = data.split()
+            buf['Roll'] = [data[1]]
+            buf['Pitch'] = [data[3]]
+            buf['Yaw'] = [data[5]]
+
+            # Dumps into json file
+            json.dump(buf, f)
 
 client_sock.close()
 server_sock.close()
