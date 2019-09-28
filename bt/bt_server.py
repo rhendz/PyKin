31e10615b6b73866ef2407ec70ae1c8582ae5687
Ankush Patel <apatel79@vols.utk.edu>
2019-09-28 05:31:58 -0400

    update bluetooth

---
+++ bt/bt_server.py
@@ -0,0 +1,12 @@
+# Uses Bluez for Linux
+#
+# sudo apt-get install bluez python-bluez
+# 
+# Taken from: https://people.csail.mit.edu/albert/bluez-intro/x232.html
+# Taken from: https://people.csail.mit.edu/albert/bluez-intro/c212.html
+
+import bluetooth
+from bt import *   
+
+# Server Mode
+receiveMessages();
